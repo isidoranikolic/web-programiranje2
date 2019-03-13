1a592c35ea649ee57fdbfbb5b1362f562c3a5bb7
isidoranikolic <isidora.nikolic.167.15@ict.edu.rs>
2019-03-13 23:38:42 +0100

    json

--- js/sadrzaj.js
+++ js/sadrzaj.js
@@ -16,22 +16,9 @@ function generisiMeniMobilni(menu) {
     document.getElementById("menu-mobile").innerHTML += menuItems;
 }
 
-var slider = [
-    {
-        img: "img/mismo.png",
-        alt: "Mi smo kris kom"
-    },
-    {
-        img: "img/mismo1.png",
-        alt: "Obrada metala"
-    },
-    {
-        img: "img/mismo2.png",
-        alt: "Obrada polimera"
-    }
-]
 
-function generisiSlider() {
+
+function generisiSlider(slider) {
     var sliderItems = ""
     for (var i = 0; i < slider.length; i++) {
         sliderItems += '<li><img src="' + slider[i].img + '" alt="' + slider[i].alt + '" /></li>'
@@ -39,22 +26,9 @@ function generisiSlider() {
     document.getElementById("slider-list").innerHTML += sliderItems;
 }
 
-var pocetnaTekst = [
-    {
-        class: "col-md-5 col-md-offset-1 col-sm-12",
-        id: "levop",
-        naslov: "ISTORIJAT",
-        tekst: "Firma Kris-kom nastala je 1993. godine u Novoj Pazovi kao mala strugarska radionica. Vremenom je radionica evoluirala u alatnicu i proizvodni pogon koji sarađuje sa firmama širom sveta. Kao porodična firma Kris-kom nastoji da napreduje ali i održava porodične vrednosti koje su ključni deo naše filozofije od samog nastanka."
-    },
-    {
-        class: "col-md-5 col-sm-12",
-        id: "desnop",
-        naslov: "O NAMA",
-        tekst: "Kris-kom je porodična firma iz Nove Pazove. Firma se bavi proizvodnjom i trgovinom proizvoda od polimera kao i uslugama mašinske obrade. Ponosni smo na višedecenijsko iskustvo u izradi alata za sve vrste obrade plastike, gume i metala. Uz mašinsku obradu takođe nudimo i usluge injekcionog presovanja."
-    }
-]
 
-function generisiParagraf() {
+
+function generisiParagraf(pocetnaTekst) {
     var paragraf = ""
     for (var i = 0; i < pocetnaTekst.length; i++) {
         paragraf += '<div class="' + pocetnaTekst[i].class + '" id="' + pocetnaTekst[i].id + '"><h5 class="col-md-12">' + pocetnaTekst[i].naslov + '</h5><p class="wow fadeInDown">' + pocetnaTekst[i].tekst + '</p></div>'
@@ -62,7 +36,7 @@ function generisiParagraf() {
     document.getElementById("paragraf-tekst").innerHTML += paragraf;
 }
 
-var uslugeItems = [
+var uslugeItems=[
     {
         slikaPozadina: "img/u1.jpg",
         slikaPozadinaAlt: "Pozadina rezanje",
@@ -103,59 +77,14 @@ function generisiUslugu(usluga) {
 }
 
 
- /*const searchBar=document.forms['tbFilter'].querySelector('input');
- searchBar.addEventListener('keyup',function(e){
-    const term=e.target.value.toLowerCase();
-    const usluge=uslugeItems.slikaPozadinaAlt;
-    Array.from(uslugeItems).forEach(function(usluga){
-        const naziv=uslugeItems.slikaNaslovAlt;
-        if(naziv.toLowerCase().indexOf(term)!=-1){
-            usluga.style.displey='block';
-        }
-        else{
-            usluga.style.displey='none';
-        }
-    })
- })
-*/
-
-var aboutItems = {
-    slika:
-        {
-            src: "img/isidora.jpg",
-            klasa: "about__image",
-            alt: "isidora"
-        },
-    tekst: "Moje ime je Isidora Nikolić, živim u Novoj Pazovi. Pohađam Visoku ICT školu, smer internet tehnologije."
-}
-
-function generisiAbout() {
+function generisiAbout(aboutItems) {
     var about = ' <div class="col-md-5 col-md-offset-1"><img  src="' + aboutItems.slika.src + '" alt="' + aboutItems.slika.alt + '" class="' + aboutItems.slika.klasa + '"></div><div class="col-md-4 col-md-offset-1 desno wow fadeInRight"><div class="row"><p>' + aboutItems.tekst + '</p></div></div>'
     document.getElementById("aboutid").innerHTML += about;
 }
 
-var kontaktItems = [
-    {
-        img: "img/adresa.png",
-        naslov: "Adresa",
-        tekst: "Banovacka 46, Nova Pazova",
-        alt: "Piktogram adrese"
-    },
-    {
-        img: "img/telefon.png",
-        naslov: "Telefon",
-        tekst: "+381 64/313 78 39",
-        alt: "Piktogram telefona"
-    },
-    {
-        img: "img/email.png",
-        naslov: "Email",
-        tekst: "kriskom@gmail.com",
-        alt: "Piktogram email"
-    }
-]
 
-function generisiKontakt() {
+
+function generisiKontakt(kontaktItems) {
     var kontakt = ""
     for (var i = 0; i < kontaktItems.length; i++) {
         kontakt += '<div class="row"><img src="' + kontaktItems[i].img + '" alt="' + kontaktItems[i].alt + '" /><p>' + kontaktItems[i].naslov + '</p><p>' + kontaktItems[i].tekst + '</p></div>'
@@ -163,26 +92,8 @@ function generisiKontakt() {
     document.getElementById("kontakt").innerHTML += kontakt;
 }
 
-var galleryItems = [
-    {
-        img: "img/1.jpg",
-        alt: "Kriskom"
-    },
-    {
-        img: "img/2.jpg",
-        alt: "Kriskom"
-    },
-    {
-        img: "img/3.jpg",
-        alt: "Kriskom"
-    },
-    {
-        img: "img/4.jpg",
-        alt: "Kriskom"
-    }
-]
 
-function generisiGaleriju() {
+function generisiGaleriju(galleryItems) {
     var gallery = ""
     for (var i = 0; i < galleryItems.length; i++) {
         gallery += ' <div class="col-md-3"><div class="thumbnail"><a href="' + galleryItems[i].img + '" data-lightbox="roadtrip"><img src="' + galleryItems[i].img + '" alt="' + galleryItems[i].alt + '" style="width:100%"></a></div></div>'
@@ -241,21 +152,46 @@ function resetujUsluge() {
 
 function stranica() {
     if (/index/.test(window.location.href) || window.location.pathname ==='/web-programiranje2/') {
-        generisiParagraf();
-        generisiSlider();
+        $.ajax({
+            url: 'data/slider.json',
+            success: function (json) {
+                generisiSlider(json);
+            }
+        });
+        $.ajax({
+            url: 'data/pocetnaTekst.json',
+            success: function (json) {
+                generisiParagraf(json);
+            }
+        });
     } else if (/usluge/.test(window.location.href)) {
         //f-je za usluge page
         generisiUslugu();
     } else if (/kontakt/.test(window.location.href) || /kontakt1/.test(window.location.href)) {
         //f-je za kontakt page
-        generisiKontakt();
+        $.ajax({
+            url: 'data/kontakt.json',
+            success: function (json) {
+                generisiKontakt(json);
+            }
+        });
     }
     else if (/gallery/.test(window.location.href)) {
         //f-je za kontakt page
-        generisiGaleriju();
+        $.ajax({
+            url: 'data/galery.json',
+            success: function (json) {
+                generisiGaleriju(json);
+            }
+        });
     } else {
         //f-je za sve ostale (autor page)
-        generisiAbout();
+        $.ajax({
+            url: 'data/about.json',
+            success: function (json) {
+                generisiAbout(json);
+            }
+        });
     }
 }
 stranica();
@@ -280,7 +216,7 @@ function odgovorNe() {
 }
 
 $.ajax({
-    url: 'footer.json',
+    url: 'data/footer.json',
     success: function (json) {
         var footer = '<span>' + json.text + '<a href="' + json.link + '" target="_blank"> ' + json.ime + '</a></span><a href="' + json.url + '"> ' + json.imeLinka + '</a>'
         document.getElementById("footer").innerHTML += footer;
@@ -290,7 +226,7 @@ $.ajax({
 
 
 $.ajax({
-    url: 'desktopMenu.json',
+    url: 'data/desktopMenu.json',
     success: function (json) {
         generisiMeniDesktop(json);
         generisiMeniMobilni(json)
